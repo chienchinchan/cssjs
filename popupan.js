d7d79834c2e5c2e2192ce09b1fb4d0c8cf83a275
chienchinchan <chien.ledai@gmail.com>
2016-08-22 14:18:28 +0700

    Update popupan.js

--- popupan.js
+++ popupan.js
@@ -1,5 +1,5 @@
 
-	var url_popup = 'https://www.facebook.com/cdrstorepro/';
+	var url_popup = 'https://www.facebook.com/khobonhalamlongan/';
 
       //<![CDATA[
 	  // var url_ppp = 'http://bit.ly/1KmPNYe';
